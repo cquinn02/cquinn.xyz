dc73d057198e91e162d3ad697254c085a6d08b6a
Conor <cmq2002@gmail.com>
2021-06-15 02:38:22 +0100

    Sass configured

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "./index.scss";
 
 const app = (): JSX.Element => {
     return (
-        <div>
-            <h1>bing bong</h1>
+        <div className="app">
+            <h1>Conor Quinn</h1>
         </div>
     );
 }
